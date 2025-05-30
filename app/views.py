c3ecc3d6428c0f8d98a9b81b228d01990f7c7102
Elias-Lipovetzky <eliaslipovetzky23@gmail.com>
2025-05-29 21:22:53 -0300

    agrego autenticacion de usuario en home

--- app/views.py
+++ app/views.py
@@ -8,7 +8,12 @@ from django.views import View
 
 
 class HomeView(TemplateView):
-    template_name = "home.html"
+    template_name = 'home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_authenticated'] = self.request.user.is_authenticated
+        return context
 
 
 class EventListView(ListView):
