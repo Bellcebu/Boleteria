2bc5bb7c0cf2c8fa1500f49718ff7cb718b3cfc6
Bellcebu <alejandroku001@gmail.com>
2025-06-19 20:43:45 -0300

    auth

--- app/static/js/auth.js
+++ app/static/js/auth.js
@@ -1,20 +1,34 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const goRight = document.getElementById("goRight");
-    const goLeft = document.getElementById("goLeft");
-    const slideBox = document.getElementById("slideBox");
-    const topLayer = document.querySelector(".topLayer");
+  const goRight = document.getElementById("goRight");
+  const goLeft = document.getElementById("goLeft");
+  const slideBox = document.getElementById("slideBox");
+  const topLayer = document.querySelector(".topLayer");
 
-    goRight?.addEventListener("click", function () {
-      slideBox.style.marginLeft = "0";
-      topLayer.style.marginLeft = "100%";
-    });
+  const activeForm = slideBox.getAttribute("data-active-form");
 
-    goLeft?.addEventListener("click", function () {
-      if (window.innerWidth > 769) {
-        slideBox.style.marginLeft = "50%";
-      } else {
-        slideBox.style.marginLeft = "20%";
-      }
-      topLayer.style.marginLeft = "0";
-    });
+  if (activeForm === "signup") {
+    if (window.innerWidth > 769) {
+      slideBox.style.marginLeft = "50%";
+    } else {
+      slideBox.style.marginLeft = "20%";
+    }
+    topLayer.style.marginLeft = "0";
+  } else {
+    slideBox.style.marginLeft = "0";
+    topLayer.style.marginLeft = "100%";
+  }
+
+  goRight?.addEventListener("click", function () {
+    slideBox.style.marginLeft = "0";
+    topLayer.style.marginLeft = "100%";
   });
+
+  goLeft?.addEventListener("click", function () {
+    if (window.innerWidth > 769) {
+      slideBox.style.marginLeft = "50%";
+    } else {
+      slideBox.style.marginLeft = "20%";
+    }
+    topLayer.style.marginLeft = "0";
+  });
+});
