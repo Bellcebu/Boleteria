84272cf37bab024eab27e92d656eaf24a291e351
Bellcebu <alejandroku001@gmail.com>
2025-06-23 04:44:09 -0300

    bug en setting

--- eventos/settings.py
+++ eventos/settings.py
@@ -103,34 +103,24 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 
-STATIC_URL = "static/"
-BASE_DIR = Path(__file__).resolve().parent.parent
+STATIC_URL = "/static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
+
 AVATAR_UPLOAD_DIR = 'avatars/'
-
-
-# Bootstrap 5
-STATICFILES_DIRS = [BASE_DIR / "static/"]
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = "es-ar"
 SESSION_EXPIRE_AT_BROWSER_CLOSE = True
 
-USE_TZ = True
+LANGUAGE_CODE = "es-ar"
 TIME_ZONE = 'America/Argentina/Buenos_Aires'
 
 USE_I18N = True
-
 USE_TZ = True
-
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
-
-STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
